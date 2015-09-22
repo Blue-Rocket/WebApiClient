d4bbfa9cbc13525cd6b305af2e44e4133a30eb94
Matt Magoffin <git+matt@msqr.us>
2015-09-23 06:44:05 +1200

    Update BRLocalize pod.

--- WebApiClient/Pods/BRLocalize/BRLocalize/Code/Core/NSObject+BRLocalize.h
+++ WebApiClient/Pods/BRLocalize/BRLocalize/Code/Core/NSObject+BRLocalize.h
@@ -1,6 +1,6 @@
 //
 //  NSObject+BRLocalizableUI.h
-//  WebApiClient
+//  BlueRocketFuelCore
 //
 //  Created by Matt on 17/08/15.
 //  Copyright (c) 2015 Blue Rocket. Distributable under the terms of the Apache License, Version 2.0.
